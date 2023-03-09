0d15147e0254de7a8138eea21b05770cfd2b0517
guicrispa <guicrispim1000@gmail.com>
2023-03-08 22:32:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-parouimpar
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-vrun5n)
---
+++ src/flow/flow.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-flow',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './flow.component.html',
+  styleUrls: ['./flow.component.css'],
+})
+export class FlowComponent implements OnInit {
+  n: number = 10;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  getNumbers() {
+    let arr = [];
+    for (let i = 1; i <= this.n; i++) {
+      arr.push(Math.trunc(100 * Math.random()));
+    }
+    return arr;
+  }
+}
